05d269e2e8161b85e61afca1aced8419cded9db1
tanakorn0314 <tanakorn0412@gmail.com>
2018-03-02 06:46:44 +0700

    fin

--- Main.java
+++ Main.java
@@ -1,25 +1,26 @@
 import java.io.*;
+import java.util.*;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
+		ArrayList<Person> pl = new ArrayList<Person>();
+
 		Person p = new Person("Person",30);
-		p.printDetails();
-
-		Student st = new Student("Student",20,"CoE",3.00f);
-		st.printDetails();
-
 		Teacher t = new Teacher("Teacher",50,"ENG");
-		t.printDetails();
-
 		Employee e = new Employee("Employee",33,"EM001");
-		e.printDetails();
-
 		PermanentEmployee pe = new PermanentEmployee("Permanent Employee",35,"EM002",15000);
-		pe.printDetails();
-
 		ContractEmployee ce = new ContractEmployee("Contract Employee",40,"EM003",9000);
-		ce.printDetails();
+
+		pl.add(p);
+		pl.add(t);
+		pl.add(e);
+		pl.add(pe);
+		pl.add(ce);
+
+		for(Person p1:pl)
+			p1.printDetails();
+
 	}
 }
--- Person.java
+++ Person.java
@@ -3,6 +3,11 @@ public class Person{
 	private String name;
 	private int age;
 
+	public Person()
+	{
+		
+	}
+
 	public Person(String name, int age)
 	{
 		this.name = name;
